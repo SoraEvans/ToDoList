dc0d79e380c8f0b193db279306bbb7119e5f8e69
Valeriya <sleepstudent@mail.ru>
2023-04-23 22:29:58 +0300

    - Доработана пагинация - Добавлены табы состояния - Добавлена кнопка удаления задач - Добавлена проверка на служебные символы при вводе

--- js/main.js
+++ js/main.js
@@ -2,29 +2,48 @@ const form = document.querySelector("#form");
 const taskInput = document.querySelector("#taskInput");
 const tasksList = document.querySelector("#tasksList");
 const paginationList = document.querySelector(".pagination");
+const removeTasks = document.querySelector("#removeTasks");
+const listStatus = document.querySelector("#listStatus");
+const removeBtn = document.querySelector("#removeBtn");
 
 let allTasks = [];
 let actualTasks = [];
+let filteredTasks = [];
 const elementsOnPage = 5;
 let page = 1;
 
-const renderTask = () => {
-  const countOfPages = Math.ceil(allTasks.length / elementsOnPage)
-  if (page > countOfPages) {
-    page = countOfPages
-  }
-  let start = (page - 1) * elementsOnPage;
-  let end = start + elementsOnPage;
-  let pages = Array.from({ length: countOfPages }, (_, idx) => `${++idx}`);
+const renderTask = (navigate) => {
+    const savedStatus = localStorage.getItem('listStatus');
+    if (savedStatus === 'Готовые') {
+        filteredTasks = allTasks.filter(task => task.done);
+    } else if (savedStatus === 'Не готовые') {
+        filteredTasks = allTasks.filter(task => !task.done);
+    } else {
+        filteredTasks = allTasks;
+    }
 
-  actualTasks = allTasks.slice(start, end);
+    const countOfPages = Math.ceil(filteredTasks.length / elementsOnPage)
+    if (page > countOfPages && countOfPages !== 0) {
+        page = countOfPages
+    }
+    if (page < countOfPages && !navigate) {
+        page = countOfPages
+    }
 
-  const pagesHTML = pages.map((i) => `<li class="page-item ${page === i && 'active'}"><a class="page-link" href="#">${i}</a></li>`)
-  paginationList.innerHTML = pagesHTML.join('')
+    let start = (page - 1) * elementsOnPage;
+    let end = start + elementsOnPage;
+    let pages = Array.from({length: countOfPages}, (_, idx) => `${++idx}`);
 
-  const taskHTML = actualTasks.map(task => {
-    const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
-    return `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+    actualTasks = filteredTasks.slice(start, end);
+
+    const pagesHTML = pages.map((_, i) => `<li class="page-item ${+page === i + 1 && 'active'}">
+                                                         <a class="page-link" href="#">${i + 1}</a>
+                                                         </li>`)
+    paginationList.innerHTML = countOfPages <= 1 ? '' : pagesHTML.join('')
+
+    const taskHTML = actualTasks.map(task => {
+        const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+        return `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
             <span class="${cssClass}">${task.text}</span>
             <div class="task-item__buttons">
                 <button type="button" data-action="done" class="btn-action">
@@ -35,77 +54,82 @@ const renderTask = () => {
                  </button>
              </div>
         </li>`
-  })
+    })
+    tasksList.innerHTML = taskHTML.join('');
 
-  tasksList.innerHTML = taskHTML.join('');
+    const removeBtnHTML = `<button id="removeTasks" class="btn btn-secondary float-right">
+                         Удалить ${savedStatus.toLowerCase()}
+                         </button>`
+    removeBtn.innerHTML = filteredTasks.length ? removeBtnHTML : '';
 }
 
 if (localStorage.getItem('allTasks')) {
-  allTasks = JSON.parse(localStorage.getItem('allTasks'));
-  renderTask()
+    allTasks = JSON.parse(localStorage.getItem('allTasks'));
+    renderTask()
 }
 
 const saveToLocalStorage = () => {
-  localStorage.setItem('allTasks', JSON.stringify(allTasks));
+    localStorage.setItem('allTasks', JSON.stringify(allTasks));
 }
 
 const checkEmptyList = () => {
-  if (allTasks.length === 0) {
-    const emptyListHTML =
-      `<li id="emptyList" class="list-group-item empty-list">
+    if (filteredTasks.length === 0) {
+        const emptyListHTML =
+            `<li id="emptyList" class="list-group-item empty-list">
              <img src="../img/leaf.svg" alt="Empty" width="48" class="mt-3">
              <div class="empty-list__title">Список дел пуст</div>
              </li>`;
-    tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
-  }
-  if (allTasks.length > 0) {
-    const emptyListEl = document.querySelector("#emptyList");
-    emptyListEl ? emptyListEl.remove() : null;
-  }
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+    }
+    if (filteredTasks.length > 0) {
+        const emptyListEl = document.querySelector("#emptyList");
+        emptyListEl ? emptyListEl.remove() : null;
+    }
 }
 
 const addTask = (event) => {
-  event.preventDefault();
-  const taskText = taskInput.value;
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    done: false,
-  }
+    event.preventDefault();
+    // Замена служебных символов при вводе
+    const taskText = taskInput.value.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    }
 
-  allTasks.push(newTask);
-  saveToLocalStorage();
-  renderTask();
+    allTasks.push(newTask);
+    saveToLocalStorage();
+    renderTask();
 
-  taskInput.value = '';
-  taskInput.focus();
+    taskInput.value = '';
+    taskInput.focus();
 
-  checkEmptyList();
+    checkEmptyList();
 }
 
 const deleteTask = (event) => {
-  if (event.target.dataset.action !== 'delete') return;
-  const parentNode = event.target.closest('li');
-  const id = Number(parentNode.id);
-  allTasks = allTasks.filter((task) => task.id !== id);
-  saveToLocalStorage();
+    if (event.target.dataset.action !== 'delete') return;
+    const parentNode = event.target.closest('li');
+    const id = Number(parentNode.id);
+    allTasks = allTasks.filter((task) => task.id !== id);
+    saveToLocalStorage();
 
-  parentNode.remove();
-  renderTask();
-  checkEmptyList();
+    parentNode.remove();
+    renderTask();
+    checkEmptyList();
 }
 
 const doneTask = (event) => {
-  if (event.target.dataset.action !== 'done') return;
-  const parentNode = event.target.closest(".list-group-item");
-  const id = Number(parentNode.id);
-  const task = allTasks.find((task) => task.id === id);
-  task.done = !task.done;
+    if (event.target.dataset.action !== 'done') return;
+    const parentNode = event.target.closest(".list-group-item");
+    const id = Number(parentNode.id);
+    const task = allTasks.find((task) => task.id === id);
+    task.done = !task.done;
 
-  saveToLocalStorage();
+    saveToLocalStorage();
 
-  const taskTitle = parentNode.querySelector(".task-title");
-  taskTitle.classList.toggle('task-title--done');
+    const taskTitle = parentNode.querySelector(".task-title");
+    taskTitle.classList.toggle('task-title--done');
 }
 
 checkEmptyList();
@@ -114,6 +138,17 @@ form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
 paginationList.addEventListener('click', (event) => {
-  page = (event.target.text)
-  renderTask()
+    page = (event.target.text)
+    renderTask(true)
+});
+removeTasks?.addEventListener('click', () => { //todo ПОЧИНИТЬ
+    localStorage.removeItem('allTasks');
+    allTasks = [];
+    renderTask();
+    checkEmptyList();
+})
+listStatus.addEventListener('click', (event) => {
+    localStorage.setItem('listStatus', event.target.value);
+    renderTask();
+    checkEmptyList();
 });
